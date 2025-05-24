d330c196e3988fe5116b8fb6e427d0b7c93b65f5
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2025-05-24 14:18:49 +0530

    correction in dubmittion

--- controllers/surveyControllers/surveySubmittion.js
+++ controllers/surveyControllers/surveySubmittion.js
@@ -4,14 +4,13 @@ const surveyModel = require('../../models/survey');
 module.exports.submit = async (req, res, next) => {
     const { data } = req.body;
     const { externalOperations, expenses, attendance, inventory, tenderVendor, departmentalBudget, boardCompanyActivity, fragmanted, manualWork, highCost, consultants, complex, expensive, outdated, expectations, adopt, reduceTime, noCustomizationCost, reduceExpenditure, selfManaged, earlyAdopter, user } = data;
-    console.log(user);
     try {
         let userSubmit = null;
         if(user) {
             userSubmit = await userModel.create(user);
         }
         const surveySubmit = await surveyModel.create({
-            respondent: userSubmit._id,
+            respondent: userSubmit?._id || null,
             externalOperations,
             expenses,
             attendance,
