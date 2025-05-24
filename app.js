8407c7f69d876a8514c823a05395029cf424ebc2
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2025-05-24 12:36:10 +0530

    corrected the views

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const donationsRotuer = require('./routes/donation');
 
 app.use(logger('dev'))
 app.set("view engine", "ejs")
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
